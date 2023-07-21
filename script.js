e17ad3dbe82aff91662d6ede98369eaae6419942
CESAR LEON <cesar.leon@mykenzie.snhu.edu>
2023-07-21 17:35:04 -0400

    add button

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // Write a function if you like!
+let colorBtn = document.getElementById("colorme")
+addEventListener("click", changeColor);
+function changeColor(){
+    colorBtn.classList.add("color")
+}
